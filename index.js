5533662c9bab3e2cb154c9a70aa6b47778d01448
Benjamin <bnjackson951@gmail.com>
2024-01-24 17:38:37 +0000

    Add strucure to JS file

---
+++ index.js
@@ -0,0 +1,29 @@
+"use strict";
+const submitBtn = document.querySelector('#formSubmitBtn');
+const deleteBtn = document.querySelectorAll('.book-delete-btn');
+
+let library = [];
+
+function getNewBook() {
+
+}
+
+function Book() {
+
+}
+
+function addBookToLibrary() {
+
+}
+
+function displayBooks() {
+
+}
+
+function updateLibraryInfo() {
+
+}
+
+function removeBookFromLibrary() {
+
+}
